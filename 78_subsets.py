85603127f4485dc06ccd090c67ecac7f6957039c
aidenj1808 <aidenj1808@gmail.com>
2025-05-26 16:47:15 -0600

    Add problem 78 solution.

---
+++ 78_subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        n = len(nums)
+        res, sol = [], []
+
+        def backtrack(i):
+            if i == n:
+                res.append(sol[:])
+                return
+            
+            # Don't pick nums[i]            
+            backtrack(i + 1)
+
+            # Pick nums[i]
+            sol.append(nums[i])
+            backtrack(i + 1)
+            sol.pop()
+        
+        backtrack(0)
+        return res
+        
