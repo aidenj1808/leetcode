4a1940b31312f725385a02cea60f1d13a8b717a1
aidenj1808 <aidenj1808@gmail.com>
2025-05-26 13:10:01 -0600

    Add problem 2864 solution.

---
+++ 2864_maximum_odd_binary_number.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        counts = Counter(s)
+        return ('1' * (counts['1'] - 1)) + ('0' * counts['0']) + "1"
+
