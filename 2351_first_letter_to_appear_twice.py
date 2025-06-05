36642b9832485126e2e76de8950cc876a2ee3ed3
aidenj1808 <aidenj1808@gmail.com>
2025-06-05 12:02:29 -0600

    Add problem 2351 solution.

---
+++ 2351_first_letter_to_appear_twice.py
@@ -0,0 +1,9 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        seen = set()
+        for char in s:
+            if char not in seen:
+                seen.add(char)
+            else:
+                return char
+        return "" 
